ba5176a7ab829fe08bc281657f993b3f13707f93
hikamizan <hikamizan771@gmail.com>
2025-03-02 13:49:08 +0700

    add enum study

---
+++ Enum/Program.cs
@@ -0,0 +1,33 @@
+﻿
+public enum UserRole
+{
+    Admin = 1,
+    User = 2,
+    Guest = 3
+}
+
+class Program
+{
+    static void Main()
+    {
+        // UserRole role = UserRole.Admin;
+
+        // using casting
+        int intRole = 2;
+        UserRole role = (UserRole)intRole;
+
+        if (role == UserRole.Admin)
+        {
+            Console.WriteLine("User Admin");
+        }
+        else if (role == UserRole.User)
+        {
+            Console.WriteLine("User Regular");
+        }
+        else if (role == UserRole.Guest)
+        {
+            Console.WriteLine("User Guest");
+        }
+    }
+}
+
