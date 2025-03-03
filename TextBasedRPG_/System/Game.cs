f993c62dc1fc06db18646daf349ac7973cfc2965
hikamizan <hikamizan771@gmail.com>
2025-03-03 14:21:56 +0700

    create state.cs and add the class to game.cs

--- TextBasedRPG_/System/Game.cs
+++ TextBasedRPG_/System/Game.cs
@@ -2,13 +2,20 @@ using System;
 
 public class Game
 {
-    bool end;
+    private bool end
+    {
+        get {return this.end;}
+        set {this.end = value;}
+    }
+    private Stack<State> states; 
+    
     private void InitialVar()
     {
         this.end = false; // this means the var belong to class not local
     }
     public Game()
     {
+        this.InitialVar();
         Console.WriteLine("Game Class");
     }
     public void Run()
