eae765bb625684d28c6c140e6ab62ef611a0c483
hikamizan <hikamizan771@gmail.com>
2025-03-03 14:13:35 +0700

    update template for game.cs

--- TextBasedRPG_/System/Game.cs
+++ TextBasedRPG_/System/Game.cs
@@ -1,8 +1,32 @@
+using System;
+
 public class Game
 {
+    bool end;
+    private void InitialVar()
+    {
+        this.end = false; // this means the var belong to class not local
+    }
+    public Game()
+    {
+        Console.WriteLine("Game Class");
+    }
     public void Run()
     {
-        // Initialize the game state
-        // GameState state = new GameState();
+        while (this.end == false)
+        {
+            Console.WriteLine("Running...");
+            string? toEnd = Console.ReadLine();
+
+            if (toEnd == "end")
+            {
+                this.end = true;
+            } else 
+            {
+                Console.WriteLine("Invalid input. Please enter 'end' to stop.");
+            }
+
+            Console.WriteLine("Game End");
+        }
     }
 }
