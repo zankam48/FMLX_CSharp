16f8f44a2adb8298fb61d23c46c0da43d8afecb0
hikamizan <hikamizan771@gmail.com>
2025-03-04 13:55:35 +0700

    update code

--- TextBasedRPG_/System/Gacha.cs
+++ TextBasedRPG_/System/Gacha.cs
@@ -1,8 +1,18 @@
 using Newtonsoft.Json;
+
+Dictionary<string, double> rarityChances = new Dictionary<string, double>()
+{
+    {"5 Star", 0.01},
+    {"4 Star", 0.10},
+    {"3 Star", 0.80},
+};
+
+// Item item = JsonConvert.DeserializeObject<Item>(jsonItem);
 public class Gacha
 {
-    static private int _pity;
-    static private int _maxPity = 90;
+    // static private int _pity;
+    // static private int Pity { get; set; }
+    // static private int _maxPity = 90;
     private static readonly Random random = new Random();
 
     public static string Roll()
@@ -10,6 +20,22 @@ public class Gacha
         int index = random.Next(0, 3); 
         // return items[index]; 
         return "pass";
+    }
+
+    public double GetRate(double pity)
+    {
+        if (pity < 75)
+        {
+            return 0.06;
+        }
+        else if (pity < 90 && pity >= 75)
+        {
+            return (pity - 75)*0.06 + 0.006;
+        }
+        else
+        {
+            return 1.00;
+        }
     }
 }
 
@@ -23,9 +49,4 @@ public class Item
     public string Description { get; set; }
 }
 
-Dictionary<string, double> rarityChances = new Dictionary<string, int>()
-{
-    {"5 Star", 0.01},
-    {"4 Star", 0.10},
-    {"3 Star", 0.80}
-}
+
